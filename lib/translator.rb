23508f29afa72583598c7d4cc5f623eb16e0303a
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-05 19:31:52 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+ #code goes here
+  emoticons = YAML.load_file(file_path)
+  emoticon_lib  = {}
+  emoticons.each do |name,arrays|
+      emoticon_lib[name] ||= {}
+      emoticon_lib[name][:english] = arrays[0]
+      emoticon_lib[name][:japanese] = arrays[1]
+  end
+emoticon_lib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoticon_lib = load_library(file_path)
+  
+  emoticon_lib.each do |meaning, values|
+  return values[:japanese] if values[:english] == emoticon
 end
+  "Sorry, that emoticon was not found"
+end 
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(file_path, emoji)
+  emoticon_lib = load_library(file_path)
+  
+  emoticon_lib.each do |meaning, values|
+  return meaning if values[:japanese] == emoji
+end
+"Sorry, that emoticon was not found"
 end
